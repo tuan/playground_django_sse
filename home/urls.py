71c532f226ed8f1a3c5498711393f622ed26686d
Tuan Nguyen <tuan@users.noreply.github.com>
2024-01-07 12:02:39 -0800

    Sends stream of timesteps to client.
    In this example, we choose to send a special message: `CLOSE` to signify the end of the stream. It's not in SSE specifications, just a convention adopted here.

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, stream
 
 urlpatterns = [
- path("", HomeView.as_view(), name="home"),
+    path("", HomeView.as_view(), name="home"),
+    path("stream/", stream, name="stream"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,33 @@
+import asyncio
+import datetime
+from django.http import HttpRequest, StreamingHttpResponse
 from django.views.generic import TemplateView
+import pytz
+
+NUMBER_EVENTS = 3
 
 
 class HomeView(TemplateView):
     template_name = "home/index.html"
+
+
+def createEvent(payload: str) -> bytes:
+    return f"data: {payload}\n\n".encode()
+
+
+async def event_stream(_request: HttpRequest):
+    for i in range(NUMBER_EVENTS):
+        await asyncio.sleep(0.5)
+        timestamp = datetime.datetime.now(pytz.timezone("America/Los_Angeles"))
+        payload = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        yield createEvent(payload=payload)
+
+    # CLOSE event is not in SSE specification
+    # this is just a convention that can be used to terminate a SSE stream
+    yield createEvent("CLOSE")
+
+
+async def stream(request: HttpRequest) -> StreamingHttpResponse:
+    return StreamingHttpResponse(
+        event_stream(request), content_type="text/event-stream"
+    )
